72a3a82e002b0d5230e1bc2449cb42574e8801be
tamtran99 <117134831+tamtran99@users.noreply.github.com>
2024-11-19 17:10:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("KHOA MỘT LẰN")
+import streamlit as st
+
+# Hiển thị link và nút
+if st.button('Click here to see "KHOA MỘT LẰN"'):
+    st.markdown('<p style="color: black; font-size: 20px;">KHOA MỘT LẰN</p>', unsafe_allow_html=True)
+else:
+    st.markdown('<a href="#" style="color: blue; font-size: 20px;">Click here to see "KHOA MỘT LẰN"</a>', unsafe_allow_html=True)
